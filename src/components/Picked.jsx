e98d29f22c2ec23df079c32c7fb04344ad5d806e
diarihza <diarihza@gmail.com>
2024-07-01 17:46:55 +0700

    fix: logic win

--- src/components/Picked.jsx
+++ src/components/Picked.jsx
@@ -1,11 +1,11 @@
-import React, {useContext, useEffect, useState} from 'react';
+import React, {useContext, useEffect, useRef, useState} from 'react';
 import RockIcon from '../assets/images/icon-rock.svg'
 import PaperIcon from '../assets/images/icon-paper.svg'
 import ScissorIcon from '../assets/images/icon-scissors.svg'
 import ChooseButton from "./ChooseButton.jsx";
 import {ScoreContext} from "../App.jsx";
 
-const list = ['paper', 'scissor', 'rock']
+const list = ['paper', 'scissor', 'rock'];
 const choices = {
   rock: { name: 'Rock', beats: 'scissor' },
   paper: { name: 'Paper', beats: 'rock' },
@@ -23,26 +23,11 @@ const determineWinner = (playerChoice, houseChoice) => {
 };
 
 const Picked = ({pick, onReset}) => {
-  const [loading, setLoading] = useState(true)
-  const [house, setHouse] = useState()
+  const [loading, setLoading] = useState(true);
+  const [house, setHouse] = useState();
   const [result, setResult] = useState('');
-  const [score, setScore] = useContext(ScoreContext)
-
-  const housePicked = () => {
-    setTimeout(() => {
-      const randomSelect = Math.floor(Math.random() * 3)
-      const matchHouse = list[randomSelect]
-
-      setHouse(Icons[matchHouse])
-      setLoading(false)
-
-      const gameResult = determineWinner(pick, matchHouse);
-      if(gameResult === 'win') {
-        setScore(() => score + 1)
-      }
-      setResult(gameResult);
-    }, 3000)
-  }
+  const [score, setScore] = useContext(ScoreContext);
+  const hasEffectRun = useRef(false);
 
   const Icons = {
     paper: {
@@ -57,43 +42,59 @@ const Picked = ({pick, onReset}) => {
       color: 'border-yellow-500',
       icon: ScissorIcon,
     }
-  }
+  };
+
+  const housePicked = () => {
+    const randomSelect = Math.floor(Math.random() * 3);
+    const matchHouse = list[randomSelect];
+
+    setHouse(Icons[matchHouse]);
+    setLoading(false);
+
+    console.log('picked', pick);
+    console.log('com', matchHouse);
+
+    const gameResult = determineWinner(pick, matchHouse);
+    if (gameResult === 'win') {
+      console.log('you win');
+      setScore(score + 1);
+    }
+    setResult(gameResult);
+  };
 
   useEffect(() => {
-    housePicked()
+    if (!hasEffectRun.current) {
+      setTimeout(housePicked, 3000);
+      hasEffectRun.current = true;
+    }
   }, []);
-
 
   return (
     <div className={'flex gap-14 p-4'}>
       <div>
         <p className={'text-center mb-8 uppercase font-semibold'}>You Picked</p>
         <div>
-          <ChooseButton icon={Icons[pick]?.icon} colorBorder={Icons[pick]?.color}/>
+          <ChooseButton icon={Icons[pick]?.icon} colorBorder={Icons[pick]?.color} />
         </div>
       </div>
-      {
-        house && (
-          <div className={'flex flex-col gap-4 items-center justify-center transition'}>
-            <p className={'text-4xl font-bold uppercase'}>YOU {result}</p>
-            <button
-              onClick={() => {onReset()}}
-              className={'rounded p-8 w-full py-4 bg-white hover:text-red-500 text-gray-700 transition uppercase font-semibold text-xs'}>play
-              again
-            </button>
-          </div>
-        )
-      }
+      {house && (
+        <div className={'flex flex-col gap-4 items-center justify-center transition'}>
+          <p className={'text-4xl font-bold uppercase'}>YOU {result}</p>
+          <button
+            onClick={onReset}
+            className={'rounded p-8 w-full py-4 bg-white hover:text-red-500 text-gray-700 transition uppercase font-semibold text-xs'}>
+            play again
+          </button>
+        </div>
+      )}
       <div>
         <p className={'text-center mb-8 uppercase font-semibold'}>The House Picked</p>
         <div className={'flex items-center justify-center'}>
-          {
-            loading ? (
-              <div className="animate-ping mt-14 md:h-20 md:w-20 h-16 w-16 rounded-full bg-sky-400 opacity-75">
-              </div>) : (
-              <ChooseButton icon={house.icon} colorBorder={house.color}/>
-            )
-          }
+          {loading ? (
+            <div className="animate-ping mt-14 md:h-20 md:w-20 h-16 w-16 rounded-full bg-sky-400 opacity-75"></div>
+          ) : (
+            <ChooseButton icon={house.icon} colorBorder={house.color} />
+          )}
         </div>
       </div>
     </div>
